a4fd23b8f17f021b70f68dacde9212497a92836a
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2025-10-12 21:07:12 -0300

    Aula 22 - Splitting webpack configuration

---
+++ 9.4 ProductionBuild_MultipleWebpackConfig/src/libs/showDate.js
@@ -0,0 +1,9 @@
+//import mnt from 'moment'; //global import on webpack.config.js
+
+export function showDate() {
+    console.log(mnt(new Date()).format("DD=MM=YYYY"));
+}
+
+export function dummyFunction() {
+    console.log("I'm a dummy function.");
+}
